4d002775f878eef6ad96c27d779603b09a8fb75e
Carlos Magno Oliveira de Abreu <magno.mabreu@gmail.com>
2021-03-17 20:36:20 -0300

    A porta 09 ja esta em uso...

--- run-docker.sh
+++ run-docker.sh
@@ -4,7 +4,7 @@ docker run --name hperf --hostname=hperf --network arcanjo \
     -e FEDERATION_NAME=ArcanjoFederation \
     -e FEDERATE_NAME=HPerf \
 	-v /etc/localtime:/etc/localtime:ro \
-	-p 36009:8080 \
+	-p 36019:8080 \
 	-d projetoarcanjo/hperf:1.0	
 
 
